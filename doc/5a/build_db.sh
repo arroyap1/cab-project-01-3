d7c27a417fbd369a3d3df85e5562a3b7c37f0262
arroyap1 <98768343+arroyap1@users.noreply.github.com>
2022-04-06 12:26:10 -0400

    Update build_db.sh

--- doc/5a/build_db.sh
+++ doc/5a/build_db.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-python3 create_db.py
-dropdb if exists "Fleet Vehicle"
+python3 build_db.py
+dropdb--if-exists "Fleet Vehicle"
 createdb "Fleet_Vehicle"
 psql -d "Fleet_Vehicle" -f "database.sql"
 psql "Fleet_Vehicle"
